4ee6c2df4e9c3182b976328d1cd1fae78558393b
Ignacio Barbano <48802984+ignaciobarbanodesigner@users.noreply.github.com>
2024-01-30 18:35:31 -0300

    code of conduct, license and readme added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Welcome to Typescale Garden
+
+Typescale Garden is a tool designed to assist you in creating, testing, and implementing your typographic scales.
+
+It comprises three main components: a client project, a plugin, and a server.
+
+## The Client
+
+This serves as the starting point for the entire process. Utilizing various controls as inputs, it calculates a typographic scale based on user configurations. The client allows fine-tuning and provides options for exporting to code, specifically in CSS format, and to design applications such as Figma through the plugin, in JSON format.
+
+## The Plugin
+
+The plugin facilitates the direct importation of the typographic scale into Figma, streamlining the often tedious process of manually creating design tokens.
+
+## The Server (Upcoming)
+
+The server is intended to function as a library for users to store all their configurations. Additionally, it offers an alternative means of connecting the client and the plugin, enabling the direct importation into Figma of the selected typographic scale from the user's account.
--- plugin/code.ts
+++ plugin/code.ts
@@ -1,52 +1,56 @@
 figma.showUI(__html__, { themeColors: true, width: 450, height: 400 });
 
 figma.ui.onmessage = async (msg) => {
-  figma.notify("Starting Plugin");
-  let currentStyles = figma.getLocalTextStyles();
-  console.log(currentStyles);
-  
+	figma.notify('Starting Plugin');
+	let currentStyles = figma.getLocalTextStyles();
+	console.log(currentStyles);
 
-  if (msg.type === "import-styles") {
-    const jsonStyles = msg.jsonStyles as Record<string, any>;
-    const fonts = [...new Set(await Object.keys(jsonStyles).map((textName)=> jsonStyles[textName]).map(({fontName})=>fontName))];
-    console.log(fonts);
-    
+	if (msg.type === 'import-styles') {
+		const jsonStyles = msg.jsonStyles as Record<string, any>;
+		const fonts = [
+			...new Set(
+				await Object.keys(jsonStyles)
+					.map((textName) => jsonStyles[textName])
+					.map(({ fontName }) => fontName)
+			)
+		];
+		console.log(fonts);
 
-    try {
-      await fonts
-      await figma.loadFontAsync({ family: "Inter", style: "Regular" });
-    } catch (error) {
-      const message = `Unable to load font: ${fonts}`;
-      figma.notify(message, { error: true });
-      console.error(message, fonts, error);
-    }
+		try {
+			await fonts;
+			await figma.loadFontAsync({ family: 'Inter', style: 'Regular' });
+		} catch (error) {
+			const message = `Unable to load font: ${fonts}`;
+			figma.notify(message, { error: true });
+			console.error(message, fonts, error);
+		}
 
-    try {
-      Object.keys(jsonStyles).forEach((styleName) => {
-        const styleProps = jsonStyles[styleName];
-        let style = currentStyles.find(({ name }) => name === styleName);
+		try {
+			Object.keys(jsonStyles).forEach((styleName) => {
+				const styleProps = jsonStyles[styleName];
+				let style = currentStyles.find(({ name }) => name === styleName);
 
-        if (!style) {
-          style = figma.createTextStyle();
-          style.name = styleName;
-        }
+				if (!style) {
+					style = figma.createTextStyle();
+					style.name = styleName;
+				}
 
-        Object.keys(styleProps).forEach((property) => {
-          const avoidedProps = ["type", "fontWeight"];
-          if (!avoidedProps.includes(property)) {
-            // @ts-ignore
-            style[property] = styleProps[property];
-          }
-        });
+				Object.keys(styleProps).forEach((property) => {
+					const avoidedProps = ['type', 'fontWeight'];
+					if (!avoidedProps.includes(property)) {
+						// @ts-ignore
+						style[property] = styleProps[property];
+					}
+				});
 
-        figma.notify("Styles imported! 🌳 😄");
-      });
-    } catch (error) {
-      const message = "Unable to import styles 🙁";
-      figma.notify(message, { error: true });
-      console.error(message, fonts, error);
-    }
-  } else if (msg.type === "cancel") {
-    figma.closePlugin();
-  }
+				figma.notify('Styles imported! 🌳 😄');
+			});
+		} catch (error) {
+			const message = 'Unable to import styles 🙁';
+			figma.notify(message, { error: true });
+			console.error(message, fonts, error);
+		}
+	} else if (msg.type === 'cancel') {
+		figma.closePlugin();
+	}
 };
