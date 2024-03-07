bf8530c1a4564ca7919a8241df88f6a1004fc87b
Ignacio Barbano <48802984+ignaciobarbanodesigner@users.noreply.github.com>
2024-03-07 12:20:26 -0300

    change image and description

--- client/src/components/SvelteHead.svelte
+++ client/src/components/SvelteHead.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	export let title: string = "Typescale Garden";
 	export let description: string =
-		"A typescale creation tool. It allows you to generate a typographic scale and export css and figma tokens out of it, directly into design trough the official figma plugin. A useful tool for people working with design systems.";
-	const ogImage = "../../tg-logo.png";
+		"Typescale Garden is a typescale creation tool. It allows you to generate a typographic scale and export css and figma tokens out of it, directly into design trough the official figma plugin. A useful tool for people working with design systems.";
+	const ogImage =
+		"https://raw.githubusercontent.com/ignacio-nacho-barbano/typescale-garden/main/client/static/logo.svg";
 </script>
 
 <svelte:head>
@@ -14,7 +15,7 @@
 	<meta property="og:url" content="https://typescalegarden.uy" />
 	<meta property="og:image" content={ogImage} />
 	<meta property="og:image:secure_url" content={ogImage} />
-	<meta property="og:image:type" content="image/png" />
+	<meta property="og:image:type" content="image/svg+xml" />
 	<meta property="og:image:width" content="144" />
 	<meta property="og:image:height" content="144" />
 	<meta property="og:image:alt" content="Typescale Garden Logo" />
