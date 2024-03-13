946dedc7d11696d711e726f3a0f297ae88f98565
Ignacio Barbano <48802984+ignaciobarbanodesigner@users.noreply.github.com>
2024-03-13 13:11:20 -0300

    change preprocessor and styles injection approach

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,11 +1,12 @@
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import adapter from "@sveltejs/adapter-netlify";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(), preprocess()],
 	onwarn: (warning, handler) => {
 		if (warning.code === "css-unused-selector") {
 			return;
