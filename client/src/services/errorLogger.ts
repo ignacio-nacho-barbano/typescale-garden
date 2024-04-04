f86863ace5da4536578f1f82be9610915e4d02c6
Ignacio Barbano <48802984+ignaciobarbanodesigner@users.noreply.github.com>
2024-04-04 12:11:18 -0300

    wrap rollbar in try-catch to fix wrangler incompatibility

--- client/src/services/errorLogger.ts
+++ client/src/services/errorLogger.ts
@@ -1,21 +1,27 @@
 import { PUB_ROLLBAR_TOKEN, PUB_APP_ENV } from "$env/static/public";
 import Rollbar from "rollbar";
 
-export const rollbar = new Rollbar({
-	accessToken: PUB_ROLLBAR_TOKEN,
-	captureUncaught: true,
-	captureUnhandledRejections: true,
-	environment: PUB_APP_ENV,
-	itemsPerMinute: 10,
-	maxItems: 5,
-	payload: {
-		code_version: "1.0.0"
-	}
-});
+let rollbar: Rollbar;
+
+try {
+	rollbar = new Rollbar({
+		accessToken: PUB_ROLLBAR_TOKEN,
+		captureUncaught: true,
+		captureUnhandledRejections: true,
+		environment: PUB_APP_ENV,
+		itemsPerMinute: 10,
+		maxItems: 5,
+		payload: {
+			code_version: "1.0.0"
+		}
+	});
+} catch (e) {
+	console.log("Unable to load rollbar");
+}
 
 export const logError = (message: string, error?: Record<string, any> | unknown) => {
-	if (PUB_APP_ENV !== "dev") {
-		// rollbar.error(message, error);
+	if (PUB_APP_ENV !== "dev" && rollbar) {
+		rollbar.error(message, error);
 	} else {
 		console.error("🚨 Error Logger: ", message, error);
 	}
