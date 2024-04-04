53f41135232d68bcc34ca5d2ba5ba3397bf9383f
Ignacio Barbano <48802984+ignaciobarbanodesigner@users.noreply.github.com>
2024-04-04 09:28:27 -0300

    svletekit 1.3 => 2 migration

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,4 +1,4 @@
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "@sveltejs/adapter-cloudflare";
 import preprocess from "svelte-preprocess";
 
